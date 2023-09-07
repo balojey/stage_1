7b7653a4c01272d7162b210576ddad3f373831d2
Ademola Balogun <balotech514@gmail.com>
2023-09-07 11:31:34 +0100

    Properly edited the output to return correct utc_time

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app.add_middleware(
 async def root(slack_name: str = "", track: str = ""):
     output = {
         "current_day": datetime.today().strftime("%A"),
-        "utc_time": datetime.utcnow(),
+        "utc_time": datetime.utcnow().isoformat().rsplit(".", 1)[0] + "Z",
         "github_file_url": "https://github.com/ademolab91/stage_1/blob/main/main.py",
         "github_repo_url": "https://github.com/ademolab91/stage_1",
         "status_code": status.HTTP_200_OK,
